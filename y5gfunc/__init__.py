a7e8da92e4c4aac0029f47bbb973fbec6acc7903
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-03-13 21:58:34 +0800

    cosmetics

--- y5gfunc/__init__.py
+++ y5gfunc/__init__.py
@@ -1,10 +1,62 @@
+'''
+Yuygfgg's collection of functions for vapoursynth video filtering and encoding, written by me and others.
+
+Functions:
+    convolution,
+    maximum,
+    minimum,
+    postfix2infix,
+    SynDeband, 
+    Fast_BM3DWrapper,
+    TIVTC_VFR,
+    DBMask,
+    get_oped_mask,
+    cambi_mask,
+    kirsch,
+    retinex_edgemask,
+    rescale,
+    Descale,
+    is_stripe,
+    reset_output_index,
+    output,
+    screen_shot,
+    WobblySource,
+    load_source,
+    get_frame_timestamp,
+    clip_to_timecodes,
+    encode_audio,
+    extract_audio_tracks,
+    ProcessMode,
+    TrackConfig,
+    AudioConfig,
+    get_bd_chapter,
+    get_mkv_chapter,
+    mux_mkv,
+    encode_check,
+    subset_fonts,
+    extract_pgs_subtitles,
+    get_language_by_trackid,
+    encode_video,
+    cfl_shader,
+    ranger,
+    PickFrames
+'''
+
 from .expr import convolution, maximum, minimum, postfix2infix
-from .filter import SynDeband, Fast_BM3DWrapper, TIVTC_VFR, DBMask, get_oped_mask, cambi_mask, kirsch, retinex_edgemask, rescale, Descale, is_stripe
+from .filter import (
+    SynDeband, Fast_BM3DWrapper, TIVTC_VFR, DBMask, get_oped_mask,
+    cambi_mask, kirsch, retinex_edgemask, rescale, Descale, is_stripe
+)
 from .preview import reset_output_index, output, screen_shot
 from .source import WobblySource, load_source, get_frame_timestamp, clip_to_timecodes
-from .encode import  encode_audio, extract_audio_tracks, ProcessMode, TrackConfig, AudioConfig, get_bd_chapter, get_mkv_chapter, mux_mkv, encode_check, subset_fonts, extract_pgs_subtitles, get_language_by_trackid, encode_video
+from .encode import (
+    encode_audio, extract_audio_tracks, ProcessMode, TrackConfig, AudioConfig, get_bd_chapter, get_mkv_chapter,
+    mux_mkv, encode_check, subset_fonts, extract_pgs_subtitles, get_language_by_trackid, encode_video
+)
 from .collections import cfl_shader
 from .utils import ranger, PickFrames
+
+__version__ = "0.0.1"
 
 __all__ = [
     'convolution',
