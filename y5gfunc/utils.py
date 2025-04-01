8a4fa17597788c819455dabaee295e7f6fdf71d5
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-04-01 23:14:13 +0800

    PickFrames: use selectevery

--- y5gfunc/utils.py
+++ y5gfunc/utils.py
@@ -1,21 +1,14 @@
 from typing import List, Union
 from vstools import vs
-from vstools import core
 from pathlib import Path
 
 def ranger(start: Union[int, float], end: Union[int, float], step: Union[int, float]) -> List[Union[int, float]]:
     assert step != 0
     return list(map(lambda i: round(start + i * step, 10), range(int((end - start) / step))))
 
+# https://discord.com/channels/1168547111139283026/1168591112160690227/1356645786342920202
 def PickFrames(clip: vs.VideoNode, indices: list[int]) -> vs.VideoNode:
-    try: 
-        ret = core.akarin.PickFrames(clip, indices=indices) # type: ignore
-    except AttributeError:
-        # modified from https://github.com/AkarinVS/vapoursynth-plugin/issues/26#issuecomment-1951230729
-        new = clip.std.BlankClip(length=len(indices))
-        ret = new.std.FrameEval(lambda n: clip[indices[n]], None, clip) # type: ignore
-    
-    return ret
+    return clip.std.SelectEvery(cycle=clip.num_frames, offsets=indices)
 
 def resolve_path(path: Union[Path, str]) -> Path:
     path = Path(path).resolve()
